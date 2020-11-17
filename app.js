e2f79e24a35067a37e42d032ad9d329c7246dbef
tjfstr <tjfosterish@gmail.com>
2020-11-17 12:56:32 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express       = require('express');
+
+
+//App Config///
+app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Get landing page//
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//Start Server
+app.listen(8000, () => {
+    console.log('Education Reformed is live!')
+});
